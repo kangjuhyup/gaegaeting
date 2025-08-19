4714a32b03751a79e88a78d7cd96ffca72c17ee4
kangjuhyup <fog0510@gmail.com>
2025-08-19 20:17:07 +0900

    remove : consumer

--- packages/account/src/consumer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { UserConsumerModule } from "./user/user.consumer.module";
-
-@Module({
-    imports : [
-        UserConsumerModule
-    ],
-})
-export class ConsumerModule {}
