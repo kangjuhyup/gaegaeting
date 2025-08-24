deecb97b7f786dd50d7be2c23ca10f4d76d5d033
kangjuhyup <fog0510@gmail.com>
2025-08-24 20:44:24 +0900

    fix(auth) : 카카오 인증토큰 검증방식 수정

--- packages/account/src/auth/infrastructure/adapter/inbound/http/auth/dto/request/social-native.request.ts
+++ packages/account/src/auth/infrastructure/adapter/inbound/http/auth/dto/request/social-native.request.ts
@@ -1,16 +1,16 @@
 import { AuthToken } from "@app/auth/domain/model/auth-token";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsJWT, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class SocialLoginNativeBody {
  @ApiProperty({ description : '소셜에서 발급받은 액세스토큰', required : true })
  @IsNotEmpty()
- @IsJWT()
+ @IsString()
  private readonly accessToken: string;
 
  @ApiProperty({ description : '소셜에서 발급받은 리프레시토큰', required : true })
  @IsNotEmpty()
- @IsJWT()
+ @IsString()
  private readonly refreshToken: string;
  
  @ApiProperty({ description : '액세스 토큰 만료 시간 (초)', required : true })
