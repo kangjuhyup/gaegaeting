fac419d4abe9f25b31d11054bda0e4083febfaf7
kangjuhyup <fog0510@gmail.com>
2025-10-17 20:27:10 +0900

    refactor : go/gin -> ts/nestjs

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/kangjuhyup/gaegaeting/packages/auth
-
-go 1.23.0
---
+++ packages/auth/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/../core/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
