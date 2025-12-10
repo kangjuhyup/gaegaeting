bc49b16ea93af57cc347bdba694a6d98cea71f9c
kangjuhyup <fog0510@gmail.com>
2025-12-10 20:17:40 +0900

    move

--- packages/account/src/pet/domain/port/pet-certification.port.ts
+++
@@ -1,4 +0,0 @@
-export abstract class PetCertificationPort {
-
-    abstract checkCertifiaction(ownerName:string, certificationCode:string) : Promise<boolean>
-}
--- packages/account/src/pet/domain/port/pet-storage.port.ts
+++
@@ -1,9 +0,0 @@
-import { PresignedUrl } from "@app/common/vo/presigned-url";
-
-export abstract class PetStoragePort {
-    abstract getPresignedUrl(petId:number, no : number) : Promise<PresignedUrl>
-    
-    abstract deletePetImage(petId:number, no : number) : Promise<void>
-    
-    abstract hasMetadata(petId:number, no : number) : Promise<boolean>
-}
