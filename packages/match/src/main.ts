9fbd7d73ffcd60467e3f92afc0155cd1c6aad8a8
kangjuhyup <fog0510@gmail.com>
2026-01-02 15:59:20 +0900

    fix

--- packages/match/src/main.ts
+++ packages/match/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import { ENV_KEY } from './config/env.config';
 /**
  * 애플리케이션 부트스트랩
  */
@@ -30,7 +31,6 @@ async function bootstrap() {
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
-  fs.writeFileSync('../../docs/match/swagger-spec.json', JSON.stringify(document));
 
   SwaggerModule.setup('docs', app, document);
   
@@ -48,9 +48,9 @@ async function bootstrap() {
     credentials: true,
   });
   
-  // 환경 변수에서 포트 가져오기 (기본값: 3001)
+  // 환경 변수에서 포트 가져오기 (기본값: 3000)
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3001);
+  const port = configService.get<number>(ENV_KEY.MATCH_SERVICE_API_PORT);
   
   // 서버 시작
   await app.listen(port);
